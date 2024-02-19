b2652b64d2ac275c22e3171be2ea6f82547a8534
Ekthair_Uddin <ekthiar.cse@gmail.com>
2024-02-19 15:17:11 +0600

    19-02-24

--- Computer Programming --Subeen sir/Chapter-4/task-2.c
+++ Computer Programming --Subeen sir/Chapter-4/task-2.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
 
+    int m;
+
+    for(int i=0;i<n;i++){
+        scanf("%d",&m);
+
+        for(int j=10;j<=m;j++){
+            if(j%11==0){
+                continue;
+            }
+            printf("%d\n",j);
+        }
+    }
+    return 0;
+}
