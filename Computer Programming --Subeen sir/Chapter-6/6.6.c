8961680642c3fcc110048d1462b38b916a3579c7
Ekthair_Uddin <ekthiar.cse@gmail.com>
2024-03-07 18:07:02 +0600

    07-03-24

---
+++ Computer Programming --Subeen sir/Chapter-6/6.6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int number[5]={10,20,30,40};
+
+    printf("First element: %d\n",number[0]);
+    printf("Third element: %d\n",number[2]);
+
+    return 0;
+}
