5de46b1c40e3cbffb4535be89d913ea23fadc381
Ekthiar_uddin <contact.ekthiar@gmail.com>
2024-05-15 14:55:20 +0600

    Add files via upload
    update

--- Book-1/Chapter-13/13.1.c
+++ Book-1/Chapter-13/13.1.c
@@ -2,19 +2,47 @@
 
 int main()
 {
-    int n=3;
-
-    for(int i=n;i>=1;i--){
-        for(int j=0;j<i;j++){
-            printf("C");
-        }
-        printf("\n");
+  int n,b1,b2,x,y;
+  char ch;
+  int grid[10][10];
+  
+  for(int i=0;i<10;i++){
+    for(int j=0;j<10;j++){
+        grid[i][j]=1;
     }
-
-    for(int i=2;i<=n;i++){
-        for(int j=0;j<i;j++){
-            printf("C");
-        }
-        printf("\n");
-    }
+  }
+  
+  printf("Enter number of block cell: ");
+  scanf("%d",&n);
+  
+  printf("enter block cell: ");
+  for(int i=0;i<n;i++){
+      scanf("%d %d",&b1,&b2);
+      grid[b1][b2]=0;
+  }
+  
+  printf("enter current position: ");
+  scanf("%d %d",&x,&y);
+  
+  printf("now command:");
+  while(1){
+      scanf("%C",&ch);
+      if(ch=='S' && grid[x][y]!=0){
+          printf("final position is:%d,%d",x,y);
+          break;
+      }
+      else if(ch=='R' && grid[x][y]!=0){
+          y++;
+      }
+      else if(ch=='L' && grid[x][y]!=0){
+          y--;
+      }
+      else if(ch=='D' && grid[x][y]!=0){
+          x--;
+      }
+      else if(ch=='U' && grid[x][y]!=0){
+          x++;
+      }
+  }
+  return 0;
 }
